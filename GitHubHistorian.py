184f8b19c459d03e1d468c3cc30e4117dfac800b
aerobless <theo.winter@gmail.com>
2014-04-17 18:07:16 +0200

    Improved settings, shorter variable names

--- GitHubHistorian.py
+++ GitHubHistorian.py
@@ -3,10 +3,10 @@ import urllib, urllib2, re, json, base64, getpass, sys, os
 from subprocess import call
 
 #Program Settings (feel free to change to your liking):
-username = "aerobless"              #Your GitHub username
-currentRepo = "ToxicTodo"           #The commits you want to save before deleting the repo.
-historicRepo = "HistoricCommitData" #The repo where your historic commits will go
-historicRepoLocalPath = "/Users/theowinter/git/"+historicRepo
+username = "aerobless"                #Your GitHub username
+cRep = "ToxicTodo"                    #The commits you want to save before deleting the repo.
+hRep = "HistoricCommitData"           #The repo where your historic commits will go
+hRepPath = "/Users/theowinter/git/"   #The path to your git folder (where you keep most of your repos)
 
 #Functions:
 def getJSON( url ):
@@ -18,48 +18,49 @@ def getJSON( url ):
 
 #Main Program:
 print ""
-print " GitHubHistorian v0.1 "
-print "----------------------"
+print " GitHub Historian v0.2 "
+print "-----------------------"
+hRepPath = hRepPath+hRep #Appending the repoName
 
 useStoredSettings = raw_input("Type 'yes' if you want to enter custom settings:")
 if(useStoredSettings=="yes" or useStoredSettings=="y"):
-  currentRepo = raw_input("Which repository (name):")
+  cRep = raw_input("Which repository (name):")
   username = raw_input("Your GitHub username:")
 password = getpass.getpass("Your GitHub password: ")
 
-#Check if the HistoricRepo exists
+#Check if the hRep exists
 try:
-  historicRepoData = getJSON('https://api.github.com/repos/'+username+'/'+historicRepo+'/branches')
+  hRepData = getJSON('https://api.github.com/repos/'+username+'/'+hRep+'/branches')
 except urllib2.HTTPError:
   print ""
   print "INFORMATION:"
-  print "GitHubHistorian was unable to detect your historic repository named: "+historicRepo
+  print "GitHubHistorian was unable to detect your historic repository named: "+hRep
   print "Please login to github.com and create a new repository with that name and be sure to initalize it!"
   print "If you already have a repo with a differnet name, you can change the repository name in the settings."
   print ""
   sys.exit()
 
 #Go to local Repo
-if not os.path.exists(historicRepoLocalPath):
-  createFolder = raw_input("Your local repo-folder at '"+historicRepoLocalPath+"' doesn't exist, would you like to create it? yes/no")
+if not os.path.exists(hRepPath):
+  createFolder = raw_input("Your local repo-folder at '"+hRepPath+"' doesn't exist, would you like to create it? yes/no")
   if(createFolder == "yes" or createFolder == "y"):
-    os.makedirs(historicRepoLocalPath)
+    os.makedirs(hRepPath)
   else:
     print("You can change the location of the repo-folder in the settings if you wish. Aborting program.")
     sys.exit()
 
 
-#call(["cd", "historicRepoLocalPath"])
+#call(["cd", "hRepPath"])
 
 #Get Repo Sha Id
-repo_information_json = getJSON('https://api.github.com/repos/'+username+'/'+currentRepo+'/branches')
+repo_information_json = getJSON('https://api.github.com/repos/'+username+'/'+cRep+'/branches')
 repo_sha = repo_information_json[0]['commit']['sha']
 
 print "Your SHA-ID: "+repo_sha
 
 #We get one commit, re-commit it in our history repo
 #and then get the next commit based on it's parent-sha
-repo_commits_json = getJSON('https://api.github.com/repos/'+username+'/'+currentRepo+'/commits/'+repo_sha)
+repo_commits_json = getJSON('https://api.github.com/repos/'+username+'/'+cRep+'/commits/'+repo_sha)
 
 list = []
 for k, v in repo_commits_json.iteritems():
