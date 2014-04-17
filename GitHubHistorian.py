eb93c694fe0472a075b7dbc955b67a6572b73b00
TheoWinter <theo.winter@gmail.com>
2014-04-17 11:59:36 +0200

    implemented proper authentication to github
    so that we’re not limited by the 60queries per hours limitation.
    When logged in we can get 5000queries

--- GitHubHistorian.py
+++ GitHubHistorian.py
@@ -2,6 +2,7 @@
 #Imports:
 import urllib2, re
 import json
+import base64
 
 print "";
 print " GitHubHistorian v0.1 "
@@ -10,15 +11,28 @@ print "----------------------"
 #Settings
 repo = "ToxicTodo"
 username = "aerobless"
+password = raw_input("Enter your password: ")
+
+def getJSON( url ):
+  request = urllib2.Request(url)
+  base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+  request.add_header("Authorization", "Basic %s" % base64string)
+  return json.loads(urllib2.urlopen(request).read())
 
 #Get Repo Sha Id
-repo_information_str = urllib2.urlopen('https://api.github.com/repos/'+username+'/'+repo+'/branches')
-repo_information_json = json.loads(repo_information_str.read())
+repo_information_json = getJSON('https://api.github.com/repos/'+username+'/'+repo+'/branches')
 repo_sha = repo_information_json[0]['commit']['sha']
 
 print "Your SHA-ID: "+repo_sha
 
-#Get commits
-repo_commits_str = urllib2.urlopen('https://api.github.com/repos/'+username+'/'+repo+'/commits?per_page=100&sha='+repo_sha)
-repo_commits_json = json.loads(repo_commits_str.read())
-print repo_commits_json
+#We get one commit, re-commit it in our history repo
+#and then get the next commit based on it's parent-sha
+repo_commits_json = getJSON('https://api.github.com/repos/'+username+'/'+repo+'/commits/'+repo_sha)
+
+list = []
+for k, v in repo_commits_json.iteritems():
+  if v > 6:
+    list.append(k)
+print list
+
+#print repo_commits_json
