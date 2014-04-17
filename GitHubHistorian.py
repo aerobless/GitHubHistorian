f0b4d66d266e21a354c568d1b701b82e1712f8d3
TheoWinter <theo.winter@gmail.com>
2014-04-17 13:57:08 +0200

    Improved structure

--- GitHubHistorian.py
+++ GitHubHistorian.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 import urllib2, re, json, base64, getpass
+from subprocess import call
 
-print "";
-print " GitHubHistorian v0.1 "
-print "----------------------"
+#Program Settings (feel free to change to your liking):
+currentRepo = "ToxicTodo"         #The commits you want to save before deleting the repo.
+historicRepo = "GitHubHistorian"  #The repo where your historic commits will go
+username = "aerobless"            #Your GitHub username
 
+#Functions:
 def getJSON( url ):
   request = urllib2.Request(url)
   base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
@@ -12,26 +15,27 @@ def getJSON( url ):
   request.add_header("Authorization", "Basic %s" % base64string)
   return json.loads(urllib2.urlopen(request).read())
 
-#Settings
-useStoredSettings = raw_input("Type 'yes' if you want to use the stored settings:")
+#Main Program:
+print ""
+print " GitHubHistorian v0.1 "
+print "----------------------"
+
+useStoredSettings = raw_input("Type 'yes' if you want to enter custom settings:")
 if(useStoredSettings=="yes" or useStoredSettings=="y"):
-  repo = "ToxicTodo"
-  username = "aerobless"
-else:
   repo = raw_input("Which repository (name):")
   username = raw_input("Your GitHub username:")
 
 password = getpass.getpass("Your GitHub password: ")
 
 #Get Repo Sha Id
-repo_information_json = getJSON('https://api.github.com/repos/'+username+'/'+repo+'/branches')
+repo_information_json = getJSON('https://api.github.com/repos/'+username+'/'+currentRepo+'/branches')
 repo_sha = repo_information_json[0]['commit']['sha']
 
 print "Your SHA-ID: "+repo_sha
 
 #We get one commit, re-commit it in our history repo
 #and then get the next commit based on it's parent-sha
-repo_commits_json = getJSON('https://api.github.com/repos/'+username+'/'+repo+'/commits/'+repo_sha)
+repo_commits_json = getJSON('https://api.github.com/repos/'+username+'/'+currentRepo+'/commits/'+repo_sha)
 
 list = []
 for k, v in repo_commits_json.iteritems():
@@ -39,4 +43,7 @@ for k, v in repo_commits_json.iteritems():
     list.append(k)
 print list
 
-#print repo_commits_json
+print repo_commits_json["commit"]["author"]["name"]
+
+#Run Shell command
+call(["ls", "-l"])
